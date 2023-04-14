e969b803cd11c344c738563a7cabc0e138738c15
Mark Rizko <mark@rizkocircle.com>
2023-04-14 15:40:54 -0700

    card and deck done, game WIP

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -1,25 +1,69 @@
-use std::fmt;
+use std::fmt::Display;
+use rand::{thread_rng, seq::SliceRandom};
 
 static mut counter : i32 = 0;
 
 #[derive(Clone, Copy)]
 pub struct Card {
-    value : u8,
-    cardTag : u8,
+    value : Option<u8>,
+    cardTag : Option<u8>,
 }
 
 impl Card {
     // constructor/destructor
-    fn new() -> Card;
-    pub fn isAce(&self) -> bool;
-    pub fn getValue(&self) -> u8;
-    pub fn getTag(&self) -> u8;
-    pub fn setTag(&mut self);
+    fn new_empty() -> Self {
+        Self {
+            cardTag: None,
+            value: None,
+        }
+    }
+    fn new_full(tag: u8) -> Self{
+        let value = match tag{
+            2..=10 => Some(tag),
+            1 => Some(11),
+            11..=13 => Some(10),
+            _ => None,
+        };
+
+        Self {
+            cardTag: Some(tag),
+            value,
+        }
+    }
+    pub fn isAce(&self) -> bool {
+        match self.value{
+            Some(11) => true,
+            _ => false
+        };
+        false
+    }
+    pub fn getValue(&self) -> Option<u8>{
+        self.value
+    }
+    pub fn getTag(&self) -> Option<u8>{
+        self.cardTag
+    }
+    pub fn setTag(&mut self, tag:Option<u8>){
+        self.cardTag = tag;
+    }
     // friend bool operator==
     // friend bool operator !=
     // Card operator=
     // Card operator []
     // friend operator <<    
+}
+
+impl Display for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.cardTag {
+            Some(1) => write!(f, "A"),
+            Some(x) if x >= 2 && x <= 10 => write!(f, "{:?}", Some(self.cardTag)),
+            Some(11) => write!(f, "J"),
+            Some(12) => write!(f, "Q"),
+            Some(13) => write!(f, "K"),
+            _ => write!(f, "X")
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -29,15 +73,66 @@ pub struct Deck {
 }
 
 impl Deck{
-    fn new() -> Deck;
-    fn shuffle_deck(&mut self);
-    fn fill_red(&mut self);
-    fn fill_black(&mut self);
-    fn get_card(&self) -> Card; // gpt suggests mut self
-    fn deck_size(&self) -> u8;
-    fn count_deck(&self) -> u8;
-    fn push_front(&mut self, card: Card);
-    fn card_swap(&mut self);
-    fn clear_deck(&mut self);
+    pub fn new() -> Self{
+        Self {
+            cardsLeft: 0,
+            cDeck : Vec::new(),
+        }
+    }
+    pub fn shuffle_deck(&mut self){
+        self.cDeck.shuffle(&mut thread_rng());
+    }
+    pub fn fill_red(&mut self){
+        for i in 1..14{
+            let in_card = Card::new_full(i);
+            self.cDeck.push(in_card);
+            self.cDeck.push(in_card);
+        }
+        self.cardsLeft = 26;
+        self.shuffle_deck();
+    }
+    pub fn fill_black(&mut self){
+        for i in 1..14{
+            let in_card = Card::new_full(i);
+            self.cDeck.push(in_card);
+            if i != 13{
+                self.cDeck.push(in_card);
+            }
+        }
+        self.cardsLeft = 25;
+        self.shuffle_deck();
+    }
+    pub fn get_card(&mut self) -> Option<Card>{
+        if self.cardsLeft == 0 {
+            println!("No cards left\n");
+            return None
+        }
+        let card = self.cDeck[0];
+        let length = self.cDeck.len();
+        self.cDeck.swap(0, length - 1);
+        self.cDeck.pop();
+        self.cardsLeft -= 1;
+        return Some(card)
+    }
+    pub fn deck_size(&self) -> u8 {
+        self.cardsLeft
+    }
+    pub fn count_deck(&self) -> u8{
+        let mut count : u8 = 0;
+        for card in &self.cDeck{
+            if card.getTag() != Some(1) {
+                count += card.getValue().unwrap_or_default();
+            }
+        }
+        count
+    }
+    pub fn push_front(&mut self, card: Card){
+        self.cDeck.insert(0, card)
+    }
+    // fn card_swap(&mut self);
+    pub fn clear_deck(&mut self){
+        self.cardsLeft = 0;
+        self.cDeck.clear();
+    }
 }
 
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,5 +1,19 @@
 use crate::card::*;
 
+enum TieType {
+    Tie,
+    Win,
+    Lose
+}
+
+enum CompareResult {
+    AceWipe,
+    KingTie,
+    Invalid,
+    Tie,
+    Win
+}
+
 pub struct Game {
     adStats : bool,
     gameOver : bool,
@@ -8,9 +22,9 @@ pub struct Game {
     blackDeck : Deck,
     selectedRed : Vec<u8>,
     selectedBlack : Vec<u8>,
-    redInPlay : Vec<Card>,
+    redInPlay : Vec<Option<Card>>,
     redNumIP : u8,
-    blackInPlay : Vec<Card>,
+    blackInPlay : Vec<Option<Card>>,
     blackNumIP : u8,
     ca : u8,
     blackCount : u8,
@@ -23,6 +37,121 @@ pub struct Game {
     pub pa : bool,
 }
 
-// impl Game {
+impl Game {
+    pub fn new() -> Game{
+        let mut red_deck = Deck::new();
+        let mut black_deck = Deck::new();
+        red_deck.fill_red();
+        black_deck.fill_black();
+        let black_count = black_deck.count_deck();
+        let red_count = red_deck.count_deck();
+        let mut game = Game {
+            redDeck : red_deck,
+            blackDeck : black_deck,
+            pa : false,
+            ca : 0,
+            score : 0,
+            adStats : false,
+            redNumIP : 0,
+            blackNumIP : 0,
+            validMove : false,
+            initFlag : true, 
+            gameOver : false,
+            fEnc : false,
+            blackCount : (black_count + 32),
+            redCount : red_count,
+            selectedBlack : Vec::new(),
+            selectedRed : Vec::new(),
+            redInPlay : Vec::new(),
+            blackInPlay : Vec::new(),
+            winner : false,
+            na : 0   
+        };
+        game.draw();
+        game
+
+    }
+    pub fn draw(&mut self){
+        if (self.redDeck.deck_size() != 0){
+            for i in 0..3{
+                if self.redInPlay[i].is_none(){
+                    self.redInPlay[i] = self.redDeck.get_card();
+                }
+            }
+        }
+
+        if (self.blackDeck.deck_size() != 0){
+            for i in 0..3{
+                if self.blackInPlay[i].is_none(){
+                    self.blackInPlay[i] = self.blackDeck.get_card();
+                }
+            }
+        }
+    }
     
-// }
+    pub fn runGame(&mut self){
+        loop {
+            self.validMove = false;
+            while !self.validMove && !self.gameOver {
+                if self.adStats {
+                    // self.display_ad_stats
+                }
+                // self.display_cards();
+                // self.turn();
+                self.draw();
+            }
+            if self.gameOver {
+                break;
+            }
+        }
+        // self.end_game();
+        // self.play_again();
+    }
+
+    // TODO FIX EXPECT LOGIC
+    // TODO FINISH THIS FUNCTION after compare
+    pub fn Tie(&mut self) -> TieType{
+        if self.blackDeck.deck_size() == 0 {
+            return TieType::Win;
+        }
+
+        let red = match self.redDeck.get_card(){
+            Some(card) => card,
+            None => {
+                self.redCount
+            }
+        };
+        let black = self.blackDeck.get_card();
+
+        if !red.expect("No cards left").isAce(){
+            self.redCount -= red.expect("invalid card").getValue();
+        }
+
+        TieType::Tie
+    }
+
+    // TODO FIX
+    pub fn redTotal(&self) -> u8{
+        let mut sum : u8 = 0;
+        for i in &self.selectedRed {
+            if let Some(card) = &self.redInPlay[*i] {
+                sum += card.getValue().unwrap_or(0);
+            }
+        }
+        sum
+    }
+
+    pub fn compare(&self) -> CompareResult{
+        if self.redInPlay[0].unwrap().isAce(){
+            println!("ACE WIPE\n");
+            return CompareResult::AceWipe;
+        }
+        if (self.redInPlay[0].unwrap().getTag() == Some(13) && self.blackInPlay[0].unwrap().getTag() == Some(13)) {
+             return CompareResult::KingTie;
+        }
+
+        if (self.r)
+
+        return CompareResult::Tie
+    }
+}
